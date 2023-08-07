2147a6747d95935e826d1434cfa9aa6f5a4891df
Tatiana Carvalho <carvalho.t@aluno.ifsp.edu.br>
2023-08-07 17:55:01 -0300

    configurando o servidor

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
-console.log('Rodando servidor...');
+const express = require('express');
+const api = express();
 
-function sum (n1, n2){
-    return n1 + n2;
-}
+api.get('/', (req, res) => {
+    res.json({mensage: "Bem-vindo a nossa API"});
+});
+
+api.listen(3000, ()=> {
+    console.log('Servidor rodando na porta 3000. http://localhost:3000');
+});
