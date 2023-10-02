6261f0f72843df74602d1cd70a32863236a4ac6d
taticar <carvalho.t@aluno.ifsp.edu.br>
2023-10-02 17:51:33 -0300

    conserto pass

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -7,12 +7,12 @@ const getAll = async () => {
     return await db.query('SELECT id, name, email, photo FROM users')
 }
 const create = async (user) => {
-    const {name, email, pass} = user
-    return await db.query('INSERT INTO users (name, email, password, photo) VALUES (? , ?, ?, ?)', [name, email, password, photo] )
+    const {name, email, pass, photo} = user
+    return await db.query('INSERT INTO users (name, email, pass, photo) VALUES (? , ?, ?, ?)', [name, email, pass, photo] )
 }
 const upDateId = async (user) => {
     const {id, name, email, pass, photo} = user
-    return await db.query('UPDATE users SET name = ?, email = ?, password = ?, photo = ?   WHERE id = ?', [name, email, password, id, photo])
+    return await db.query('UPDATE users SET name = ?, email = ?, pass = ?, photo = ?   WHERE id = ?', [name, email, pass, id, photo])
 }
 const deleteId = async (id) => {
     return await db.query('DELETE FROM users WHERE id = ?', [id])
